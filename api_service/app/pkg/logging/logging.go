58ad8fa6f905ff918fd3b695169ff2c66be6ca07
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-11-24 23:22:17 +0300

    5:43

--- api_service/app/pkg/logging/logging.go
+++ api_service/app/pkg/logging/logging.go
@@ -1,1 +1,40 @@
 package logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+)
+
+// writerHook is a hook that writes logs of specified LogLevels to specified Writer
+type writeHook struct {
+	Writer []io.Writer
+	LogLevels []logrus.Level
+}
+
+// Fire will be called when some logging function is called with current hook
+// It will format log entry to string and write it to appropriate writer
+func (hook *writeHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+
+	for _, w := range hook.Writer {
+		_, err = w.Write([]byte(line))
+	}
+	return err
+}
+
+func (hook *writeHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func (l *Logger) GetLoggerWithField(k string, v interface{}) Logger {
+	return Logger{l.WithField(k, v)}
+}

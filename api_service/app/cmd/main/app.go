25c06daa0a29e2786eba3706c69b3673f23cf57c
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-11-25 13:57:38 +0300

    Config

--- api_service/app/cmd/main/app.go
+++ api_service/app/cmd/main/app.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/VasilyGorbunov/notes_system/api_service/internal/config"
+	"github.com/VasilyGorbunov/notes_system/api_service/pkg/logging"
+)
+
+func main() {
+	logging.Init()
+	logger := logging.GetLogger()
+	logger.Println("logger initialized")
+
+	logger.Println("config initializing")
+	cfg := config.GetConfig()
+}
--- api_service/app/pkg/logging/logging.go
+++ api_service/app/pkg/logging/logging.go
@@ -1,13 +1,18 @@
 package logging
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
 )
 
 // writerHook is a hook that writes logs of specified LogLevels to specified Writer
 type writeHook struct {
-	Writer []io.Writer
+	Writer    []io.Writer
 	LogLevels []logrus.Level
 }
 
@@ -35,6 +40,42 @@ type Logger struct {
 	*logrus.Entry
 }
 
+func GetLogger() Logger {
+	return Logger{e}
+}
+
 func (l *Logger) GetLoggerWithField(k string, v interface{}) Logger {
 	return Logger{l.WithField(k, v)}
+}
+
+func Init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s:%d", filename, f.Line), fmt.Sprintf("%s()", f.Function)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.MkdirAll("logs", 0755)
+	if err != nil || os.IsExist(err) {
+		panic("can't create log dir. no configured logging to files")
+	} else {
+		allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+		if err != nil {
+			panic(fmt.Sprintf("[Message]: %s", err))
+		}
+
+		l.SetOutput(ioutil.Discard)
+
+		l.AddHook(&writeHook{
+			Writer:    []io.Writer{allFile, os.Stdout},
+			LogLevels: logrus.AllLevels,
+		})
+	}
+	l.SetLevel(logrus.TraceLevel)
+	e = logrus.NewEntry(l)
 }
